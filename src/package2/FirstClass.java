52ea3118bfdc597dc328772fe64ff6ca3e010f92
Husna Ceren <hcyesilcicek61@gmail.com>
2020-08-09 13:07:03 -0300

    this is fifth commit from local master

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -11,7 +11,7 @@ public class FirstClass {
 	        System.out.println("This is the forth commit from local master");
 		
 		        
-		        System.out.println("This is the second commit from remote master");
+		    System.out.println("This is the second commit from remote master");
 		
 	}
 }
